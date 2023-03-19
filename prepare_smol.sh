fad1d8940dba7d6f3ca38c228ac6443b9b5169c7
Jakub Sadowski <github@vcc.earth>
2023-03-19 17:41:40 +0100

    resolving problems with 4MB platform and presets.json

--- prepare_smol.sh
+++ prepare_smol.sh
@@ -13,7 +13,7 @@ curl -s https://api.github.com/repos/Aircoookie/WLED/releases/latest \
 | wget -O $WLED_FW_PATH -i -
 
 # 352 = 0x160 - exact number of 4096b blocks in 0x80000 flash area
-littlefs_create -i $PIO_BIN_DIR/littlefs.bin -c 176 -s src/wled_presets
+littlefs_create -i $PIO_BIN_DIR/littlefs.bin -c 176 -s data
 
 esptool.py -t --chip ESP32 merge_bin -o release.bin --flash_mode dio --flash_size 4MB \
 0x1000 $PIO_BIN_DIR/bootloader.bin \
